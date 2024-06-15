7d0956cf126da51f12ebb0d2568c1bab363d2da9
Vigneshmanikandan98 <41299987+Vigneshmanikandan98@users.noreply.github.com>
2024-06-15 16:43:42 +0000

    outpustr

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,45 @@
-#Ec2 public ip
-output "app1PublicIP" {
-  description = "public IP"
-  value = aws_instance.app1.public_ip
+# #Ec2 public ip
+# output "app1PublicIP" {
+#   description = "public IP"
+#   value = aws_instance.app1.public_ip
+# }
+
+# #EC2 public DNS
+# output "app1PublicDns" {
+#   description = "public DNS"
+#   value = aws_instance.app1.public_dns
+# }
+
+#list
+output "instance_list" {
+  description = "instance list"
+  value = [ for ins in aws_instance.app1: ins.public_dns]
 }
 
-#EC2 public DNS
-output "app1PublicDns" {
-  description = "public DNS"
-  value = aws_instance.app1.public_dns
+#Map
+output "instance_map" {
+  description = "instance map"
+  value = {
+    for ins in aws_instance.app1: ins.id => ins.public_dns
+  }
+}
+
+#map2
+output "instance_map_count" {
+  description = "instance map 2"
+  value = {
+    for c, ins in aws_instance.app1: c => ins.public_dns
+  }
+}
+
+#splat
+output "instance_splat" {
+  description = "splat"
+  value = aws_instance.app1.*.public_dns
+}
+
+#splat 2
+output "instance_splat_co" {
+  description = "spalat"
+  value = aws_instance.app1[*].public_dns
 }
