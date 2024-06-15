272879c70fa710b16952d52be2e662dc0f528a1c
Vigneshmanikandan98 <41299987+Vigneshmanikandan98@users.noreply.github.com>
2024-06-15 16:13:26 +0000

    just

--- variables.tf
+++ variables.tf
@@ -12,8 +12,27 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
+#keypair
 variable "instance_keypair" {
   description = "Instance Key pair"
   type = string
   default = "terraform-key"
+}
+
+#instance list
+variable "instance_type_list" {
+  description = "EC2 list"
+  type = list(string)
+  default = [ "t3.micro", "t3.small" ]
+}
+
+#instance map
+variable "instance_type_map" {
+  description = "EC2 map"
+  type = map(string)
+  default = {
+    "dev" = "t3.micro"
+    "qa" = "t3.small"
+    "prod" = "t3.large"
+  }
 }
