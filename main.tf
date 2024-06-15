ca07afcb88a8b273677bc2e6ace131dbe9d457a0
Vigneshmanikandan98 <41299987+Vigneshmanikandan98@users.noreply.github.com>
2024-06-15 00:21:52 +0000

    vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+resource "aws_vpc" "staticVpcTerraform" {
+  cidr_block = "10.0.0.0/16"
+  
+}
+
+resource "aws_security_group" "allow_tls" {
+  ingress  {
+    from_port = 433
+    to_port = 433
+    protocol = "tcp"
+    cidr_blocks = ["1.2.3.4/32"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+}
+
+
 resource "aws_instance" "ec2Init" {
   ami           = "ami-08a0d1e16fc3f61ea" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
