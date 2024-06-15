424b00100a40ef68e8217e73c95e44ef70f19d2d
Vigneshmanikandan98 <41299987+Vigneshmanikandan98@users.noreply.github.com>
2024-06-15 03:44:28 +0000

    t2

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_default_vpc" "default" {
 }
 resource "aws_instance" "ec2Init" {
   ami           = "ami-08a0d1e16fc3f61ea"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
 
   tags = {
