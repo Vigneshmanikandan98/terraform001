1f39edece8b0260c63c166544e4bce216916badf
Vigneshmanikandan98 <41299987+Vigneshmanikandan98@users.noreply.github.com>
2024-06-15 05:40:36 +0000

    cic

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_security_group" "vpc_ssh" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = [ "-1" ]
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 
   tags = {
@@ -76,7 +76,7 @@ resource "aws_security_group" "vpc_web" {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_blocks = [ "-1" ]
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 
   tags = {
